1f616689ae15e27415819d1709fa1c4346c09ba4
Kazuki Matsuda <mats.kazuki@gmail.com>
2022-07-23 04:13:50 +0000

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,12 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'supabase-on-aws',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class MyStack extends Stack {
+  constructor(scope: Construct, id: string, props: StackProps = {}) {
+    super(scope, id, props);
+
+    // define resources here...
+  }
+}
+
+// for development, use account/region from cdk cli
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
+
+const app = new App();
+
+new MyStack(app, 'supabase-on-aws-dev', { env: devEnv });
+// new MyStack(app, 'supabase-on-aws-prod', { env: prodEnv });
+
+app.synth();
