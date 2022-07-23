b0a1850392cff620303af9422097d3ca9218b92d
Kazuki Matsuda <mats.kazuki@gmail.com>
2022-07-23 04:15:11 +0000

    update cdk version

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.33.0',
   defaultReleaseBranch: 'main',
   name: 'supabase-on-aws',
 
