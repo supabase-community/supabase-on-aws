550b5cfa0c0dc55de9a2f6a323afd1d2f690032c
Kazuki Matsuda <mats.kazuki@gmail.com>
2022-08-08 05:47:41 +0000

    bug fix - install old pgjwt

--- src/functions/db-init/00-initial-jwt-utils.sql
+++ src/functions/db-init/00-initial-jwt-utils.sql
@@ -1,68 +1,82 @@
 create schema if not exists extensions;
 create extension if not exists pgcrypto with schema extensions;
 
-CREATE OR REPLACE FUNCTION extensions.url_encode(data BYTEA)
-  RETURNS TEXT LANGUAGE SQL AS $$
-SELECT translate(encode(data, 'base64'), E'+/=\n', '-_');
-$$;
-
-CREATE OR REPLACE FUNCTION extensions.url_decode(data TEXT)
-  RETURNS BYTEA LANGUAGE SQL AS $$
-WITH t AS (SELECT translate(data, '-_', '+/')),
-    rem AS (SELECT length((SELECT *
-                           FROM t)) % 4) -- compute padding size
-SELECT decode(
-    (SELECT *
-     FROM t) ||
-    CASE WHEN (SELECT *
-               FROM rem) > 0
-      THEN repeat('=', (4 - (SELECT *
-                             FROM rem)))
-    ELSE '' END,
-    'base64');
-$$;
+-- from https://github.com/michelp/pgjwt/blob/master/pgjwt--0.1.0--0.1.1.sql
+CREATE OR REPLACE FUNCTION extensions.url_encode(data bytea) RETURNS text LANGUAGE sql AS $$
+    SELECT translate(encode(data, 'base64'), E'+/=\n', '-_');
+$$ IMMUTABLE;
 
 
-CREATE OR REPLACE FUNCTION extensions.algorithm_sign(signables TEXT, secret TEXT, algorithm TEXT)
-  RETURNS TEXT LANGUAGE SQL AS $$
+CREATE OR REPLACE FUNCTION extensions.url_decode(data text) RETURNS bytea LANGUAGE sql AS $$
+WITH t AS (SELECT translate(data, '-_', '+/') AS trans),
+     rem AS (SELECT length(t.trans) % 4 AS remainder FROM t) -- compute padding size
+    SELECT decode(
+        t.trans ||
+        CASE WHEN rem.remainder > 0
+           THEN repeat('=', (4 - rem.remainder))
+           ELSE '' END,
+    'base64') FROM t, rem;
+$$ IMMUTABLE;
+
+
+CREATE OR REPLACE FUNCTION extensions.algorithm_sign(signables text, secret text, algorithm text)
+RETURNS text LANGUAGE sql AS $$
 WITH
-    alg AS (
-      SELECT CASE
-             WHEN algorithm = 'HS256'
-               THEN 'sha256'
-             WHEN algorithm = 'HS384'
-               THEN 'sha384'
-             WHEN algorithm = 'HS512'
-               THEN 'sha512'
-             ELSE '' END) -- hmac throws error
-SELECT extensions.url_encode(extensions.hmac(signables, secret, (SELECT * FROM alg)));
-$$;
+  alg AS (
+    SELECT CASE
+      WHEN algorithm = 'HS256' THEN 'sha256'
+      WHEN algorithm = 'HS384' THEN 'sha384'
+      WHEN algorithm = 'HS512' THEN 'sha512'
+      ELSE '' END AS id)  -- hmac throws error
+SELECT extensions.url_encode(extensions.hmac(signables, secret, alg.id)) FROM alg;
+$$ IMMUTABLE;
 
 
-CREATE OR REPLACE FUNCTION extensions.sign(payload JSON, secret TEXT, algorithm TEXT DEFAULT 'HS256')
-  RETURNS TEXT LANGUAGE SQL AS $$
+CREATE OR REPLACE FUNCTION sign(payload json, secret text, algorithm text DEFAULT 'HS256')
+RETURNS text LANGUAGE sql AS $$
 WITH
-    header AS (
-      SELECT extensions.url_encode(convert_to('{"alg":"' || algorithm || '","typ":"JWT"}', 'utf8'))
-  ),
-    payload AS (
-      SELECT extensions.url_encode(convert_to(payload :: TEXT, 'utf8'))
-  ),
-    signables AS (
-      SELECT (SELECT *
-              FROM header) || '.' || (SELECT * FROM payload)
-  )
-SELECT (SELECT * FROM signables)
-       || '.' ||
-       extensions.algorithm_sign((SELECT * FROM signables), secret, algorithm);
-$$;
-
-
-CREATE OR REPLACE FUNCTION extensions.verify(token TEXT, secret TEXT, algorithm TEXT DEFAULT 'HS256')
-  RETURNS TABLE(header JSON, payload JSON, valid BOOLEAN) LANGUAGE SQL AS $$
+  header AS (
+    SELECT extensions.url_encode(convert_to('{"alg":"' || algorithm || '","typ":"JWT"}', 'utf8')) AS data
+    ),
+  payload AS (
+    SELECT extensions.url_encode(convert_to(payload::text, 'utf8')) AS data
+    ),
+  signables AS (
+    SELECT header.data || '.' || payload.data AS data FROM header, payload
+    )
 SELECT
-  convert_from(extensions.url_decode(r [1]), 'utf8') :: JSON                  AS header,
-  convert_from(extensions.url_decode(r [2]), 'utf8') :: JSON                  AS payload,
-  r [3] = extensions.algorithm_sign(r [1] || '.' || r [2], secret, algorithm) AS valid
-FROM regexp_split_to_array(token, '\.') r;
-$$;
+    signables.data || '.' ||
+    extensions.algorithm_sign(signables.data, secret, algorithm) FROM signables;
+$$ IMMUTABLE;
+
+
+-- from https://github.com/michelp/pgjwt/blob/master/pgjwt--0.1.1--0.2.0.sql
+CREATE OR REPLACE FUNCTION extensions.try_cast_double(inp text)
+RETURNS double precision AS $$
+  BEGIN
+    BEGIN
+      RETURN inp::double precision;
+    EXCEPTION
+      WHEN OTHERS THEN RETURN NULL;
+    END;
+  END;
+$$ language plpgsql IMMUTABLE;
+
+
+CREATE OR REPLACE FUNCTION extensions.verify(token text, secret text, algorithm text DEFAULT 'HS256')
+RETURNS table(header json, payload json, valid boolean) LANGUAGE sql AS $$
+  SELECT
+    jwt.header AS header,
+    jwt.payload AS payload,
+    jwt.signature_ok AND tstzrange(
+      to_timestamp(extensions.try_cast_double(jwt.payload->>'nbf')),
+      to_timestamp(extensions.try_cast_double(jwt.payload->>'exp'))
+    ) @> CURRENT_TIMESTAMP AS valid
+  FROM (
+    SELECT
+      convert_from(extensions.url_decode(r[1]), 'utf8')::json AS header,
+      convert_from(extensions.url_decode(r[2]), 'utf8')::json AS payload,
+      r[3] = extensions.algorithm_sign(r[1] || '.' || r[2], secret, algorithm) AS signature_ok
+    FROM regexp_split_to_array(token, '\.') r
+  ) jwt
+$$ IMMUTABLE;
