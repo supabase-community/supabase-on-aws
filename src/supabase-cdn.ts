033b036becea09ed9c3fbdc27804ad1a2820d746
Kazuki Matsuda <mats.kazuki@gmail.com>
2022-08-16 07:39:28 +0000

    add waf option

--- src/supabase-cdn.ts
+++ src/supabase-cdn.ts
@@ -1,4 +1,4 @@
-import { Duration } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import * as cf from 'aws-cdk-lib/aws-cloudfront';
 import { LoadBalancerV2Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
@@ -15,6 +15,16 @@ export class SupabaseCdn extends Construct {
     super(scope, id);
 
     const { originLoadBalancer } = props;
+
+    const dummyWebAclArn = 'arn:aws:wafv2:us-east-1:123456789012:global/webacl/this-is-dummy/00000000-0000-0000-0000-000000000000';
+    const wafWebAclArn = new cdk.CfnParameter(this, 'WafWebAclArn', {
+      description: 'WAF Web ACL ARN for CDN',
+      type: 'String',
+      default: dummyWebAclArn,
+      allowedPattern: '^arn:aws:wafv2:us-east-1:[0-9]{12}:global/webacl/[\\w-]+/[\\w-]{36}$',
+    });
+
+    const wafEnabled = new cdk.CfnCondition(this, 'WafEnabled', { expression: cdk.Fn.conditionNot(cdk.Fn.conditionEquals(wafWebAclArn, dummyWebAclArn)) });
 
     const defaultBehavior: cf.BehaviorOptions = {
       origin: new LoadBalancerV2Origin(originLoadBalancer, {
@@ -39,13 +49,14 @@ export class SupabaseCdn extends Construct {
         '*.js': { ...defaultBehavior, cachePolicy: cf.CachePolicy.CACHING_OPTIMIZED },
       },
       errorResponses: [
-        { httpStatus: 500, ttl: Duration.seconds(60) },
-        { httpStatus: 501, ttl: Duration.seconds(60) },
-        { httpStatus: 502, ttl: Duration.seconds(60) },
-        { httpStatus: 503, ttl: Duration.seconds(60) },
-        { httpStatus: 504, ttl: Duration.seconds(60) },
+        { httpStatus: 500, ttl: cdk.Duration.seconds(60) },
+        { httpStatus: 501, ttl: cdk.Duration.seconds(60) },
+        { httpStatus: 502, ttl: cdk.Duration.seconds(60) },
+        { httpStatus: 503, ttl: cdk.Duration.seconds(60) },
+        { httpStatus: 504, ttl: cdk.Duration.seconds(60) },
       ],
       enableIpv6: true,
     });
+    (this.distribution.node.defaultChild as cf.CfnDistribution).addPropertyOverride('DistributionConfig.WebACLId', cdk.Fn.conditionIf(wafEnabled.logicalId, wafWebAclArn.valueAsString, cdk.Aws.NO_VALUE));
   }
 };
