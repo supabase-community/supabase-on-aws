d3af0e71d00d683355907bd416724ec3ee0ac198
Kazuki Matsuda <mats.kazuki@gmail.com>
2022-08-23 21:47:36 +0000

    add logging

--- src/functions/ecs-force-deploy.ts
+++ src/functions/ecs-force-deploy.ts
@@ -4,14 +4,12 @@ import { EventBridgeHandler } from 'aws-lambda';
 const region = process.env.AWS_REGION;
 const cluster = process.env.ECS_CLUSTER_NAME;
 const service = process.env.ECS_SERVICE_NAME;
+const forceNewDeployment = true;
 
 export const handler: EventBridgeHandler<any, any, any> = async (_event, _context) => {
   const client = new ECSClient({ region });
-  const cmd = new UpdateServiceCommand({
-    cluster,
-    service,
-    forceNewDeployment: true,
-  });
-  await client.send(cmd);
+  const cmd = new UpdateServiceCommand({ cluster, service, forceNewDeployment });
+  const output = await client.send(cmd);
+  console.info(JSON.stringify(output));
   client.destroy();
 };
