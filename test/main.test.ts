be805e751beeb6f948c5f307efb410bfff7095f4
Kazuki Matsuda <mats.kazuki@gmail.com>
2022-09-01 21:56:55 +0000

    update test

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,10 +2,15 @@ import { App } from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { SupabaseStack } from '../src/supabase-stack';
 
+const pjPrefix = 'Supabase';
+
 test('Snapshot', () => {
   const app = new App();
-  const stack = new SupabaseStack(app, 'test');
+  const stack = new SupabaseStack(app, `${pjPrefix}-stack`, { meshEnabled: true, gqlEnabled: false });
+  const withoutMesh = new SupabaseStack(app, `${pjPrefix}-without-mesh`, { meshEnabled: false, gqlEnabled: false });
+  const graphql = new SupabaseStack(app, `${pjPrefix}-graphql`, { meshEnabled: true, gqlEnabled: true });
 
-  const template = Template.fromStack(stack);
-  expect(template.toJSON()).toMatchSnapshot();
+  expect(Template.fromStack(stack)).toMatchSnapshot();
+  expect(Template.fromStack(withoutMesh)).toMatchSnapshot();
+  expect(Template.fromStack(graphql)).toMatchSnapshot();
 });
