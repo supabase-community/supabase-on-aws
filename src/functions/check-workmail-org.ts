00617bad82eb23f953f9042f0030e111e1972f01
Kazuki Matsuda <mats.kazuki@gmail.com>
2022-09-28 12:49:58 +0000

    bug fix

--- src/functions/check-workmail-org.ts
+++ src/functions/check-workmail-org.ts
@@ -2,7 +2,7 @@ import { SESClient, GetIdentityVerificationAttributesCommand } from '@aws-sdk/cl
 import { WorkMailClient, DescribeOrganizationCommand } from '@aws-sdk/client-workmail';
 import { CdkCustomResourceIsCompleteHandler, CdkCustomResourceIsCompleteResponse } from 'aws-lambda';
 
-const checkVerificationStatus = async (region: string, identity: string) => {
+const checkVerificationStatus = async (region: string, identity: string): Promise<boolean> => {
   const client = new SESClient({ region });
   const cmd = new GetIdentityVerificationAttributesCommand({ Identities: [identity] });
   const { VerificationAttributes } = await client.send(cmd);
@@ -18,8 +18,8 @@ const checkVerificationStatus = async (region: string, identity: string) => {
 const checkOrganizationState = async (region: string, organizationId?: string): Promise<CdkCustomResourceIsCompleteResponse> => {
   const client = new WorkMailClient({ region });
   const cmd = new DescribeOrganizationCommand({ OrganizationId: organizationId });
-  client.destroy();
   const { State, Alias } = await client.send(cmd);
+  client.destroy();
   if (State != 'Active') {
     return { IsComplete: false };
   } else {
