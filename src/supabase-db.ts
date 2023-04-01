3ec96b726c89369d67216c7c082e7386ebbed0b7
Kazuki Matsuda <mats.kazuki@gmail.com>
2023-04-01 15:58:39 +0900

    fix: modify assetHash manually

--- src/supabase-db.ts
+++ src/supabase-db.ts
@@ -214,6 +214,7 @@ export class SupabaseDatabase extends Construct {
       description: 'Supabase - Database init function',
       entry: path.resolve(__dirname, 'functions/db-init/index.ts'),
       bundling: {
+        assetHash: cdk.FileSystem.fingerprint(path.resolve(__dirname, 'functions/db-init')),
         nodeModules: [
           '@databases/pg',
         ],
